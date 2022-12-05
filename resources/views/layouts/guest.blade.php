7d1517a9cc8ded5c56530409130ee3ac0ecbc557
MartaGitDaw <84068487+MartaGitDaw@users.noreply.github.com>
2022-12-05 22:51:21 +0100

    nav bien

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -18,37 +18,39 @@
 <body class="font-sans antialiased">
     <nav x-data="{ open: false }"
         class="fixed w-full top-0 border-b text-gray-700 bg-white dark:text-gray-200 dark:bg-gray-800 ">
-        <div class="fixed w-full top-0 dark:bg-gray-800 flex justify-between h-16 ">
-
-            <div class="flex mx-12">
-                <!-- Logo -->
-                <div class="shrink-0 flex items-center">
-                    <a href="{{ route('welcome') }}">
-                        {{-- <img src="{{ asset('storage/logo/logo.png') }}" class="block h-9 w-auto"> --}}
-                        <x-jet-application-mark class="block h-9 w-auto" />
-                    </a>
-                    <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                        {{-- <x-foro-nav-link href="#" :active="request()->routeIs('admin*')"> --}}
-                        <label class="text-sm font-medium leading-5 text-gray-900 dark:text-blue-500">Foro</label>
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div class="flex justify-between h-16">
+                <div class="flex">
+                    <!-- Logo -->
+                    <div class="shrink-0 flex items-center">
+                        <a href="{{ route('welcome') }}">
+                            {{-- <img src="{{ asset('storage/logo/logo.png') }}" class="block h-9 w-auto"> --}}
+                            <x-jet-application-mark class="block h-9 w-auto" />
+                        </a>
+                        <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                            {{-- <x-foro-nav-link href="#" :active="request()->routeIs('admin*')"> --}}
+                            <label class="text-sm font-medium leading-5 text-gray-900 dark:text-blue-500">Foro</label>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="py-4 px-2 flex flex-row-reverse text-sm mx-12">
-                @if (Route::has('login'))
-                    <div class="top-0 block">
-                        @auth
-                            <a href="{{ url('/dashboard') }}"
-                                class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
-                        @else
-                            <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log
-                                in</a>
-                            @if (Route::has('register'))
-                                <a href="{{ route('register') }}"
-                                    class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
-                            @endif
-                        @endauth
-                    </div>
-                @endif
+                <div class="py-4 px-2 flex flex-row-reverse text-sm mx-12">
+                    @if (Route::has('login'))
+                        <div class="top-0 block">
+                            @auth
+                                <a href="{{ url('/dashboard') }}"
+                                    class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
+                            @else
+                                <a href="{{ route('login') }}"
+                                    class="text-sm text-gray-700 dark:text-gray-500 underline">Log
+                                    in</a>
+                                @if (Route::has('register'))
+                                    <a href="{{ route('register') }}"
+                                        class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+                                @endif
+                            @endauth
+                        </div>
+                    @endif
+                </div>
             </div>
         </div>
     </nav>
